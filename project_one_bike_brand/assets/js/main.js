3592a03fdb24f1c3089ff605f54ee1c6743c0f63
Dev-shamama <148217035+Dev-shamama@users.noreply.github.com>
2024-11-01 11:47:57 +0500

    initial commit

---
+++ project_one_bike_brand/assets/js/main.js
@@ -0,0 +1,39 @@
+// Function to handle opening a model tab
+function openModelTab(event, tabId) {
+    // Deselect all brand tabs and bike selection tabs
+    document.querySelectorAll('.item-brand').forEach(brand => brand.classList.remove('active'));
+    document.querySelectorAll('.choose-bike').forEach(tab => tab.classList.remove('active'));
+
+    // Activate selected brand tab and corresponding bike selection tab
+    event.currentTarget.classList.add('active');
+    document.getElementById(tabId).classList.add('active');
+  }
+
+  // Function to handle opening model detail within a bike tab
+  function openModelDetailTab(event, tabId) {
+    // Deselect all model options and details
+    document.querySelectorAll('.item-text').forEach(item => item.classList.remove('active'));
+    document.querySelectorAll('.bike-model-content').forEach(content => content.classList.remove('active'));
+
+    // Activate selected model option and corresponding details
+    event.currentTarget.classList.add('active');
+    document.getElementById(tabId).classList.add('active');
+  }
+
+  const item_texts = document.querySelectorAll('.item-text');
+  const item_brand = document.querySelectorAll('.item-brand');
+  
+  const choose_option = document.querySelector('#choose-option');
+
+  item_brand.forEach(i => {
+    i.addEventListener('click', () => {
+      choose_option.classList.remove('active')
+    })
+  })
+  
+  item_texts.forEach(e => {
+    e.addEventListener("click", () => {
+      console.log(e);
+      choose_option.classList.add('active')
+    })
+  })
